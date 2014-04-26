2ca1736422517373a9cb9ce0eceb6f2e20b023c9
job-lukasz <job.lukasz.1990@gmail.com>
2014-04-27 00:48:31 +0200

    Refactor all Wrap methods

--- src/main/java/main/Wrap.java
+++ src/main/java/main/Wrap.java
@@ -5,7 +5,7 @@ public class Wrap {
 	private int minWidth;
 	private int wordIterator = 0;
 	private int lineWidth = 0;
-	private String newText;
+	private StringBuilder newText;
 	private String words[];
 
 	public void setMinWidth(int minWidth) {
@@ -16,17 +16,13 @@ public class Wrap {
 		this.maxWidth = maxWidth;
 	}
 
-	private void deleteLastSpaces() {
-		newText = newText.replaceAll(" +$", "");
-	}
-	
 	private void initVariables(String rawText) {
 		words = rawText.split(" ");
-		newText = "";
+		newText = new StringBuilder();
 		wordIterator = 0;
 		lineWidth = 0;
 	}
-	
+
 	private boolean isEnoughSpaceForWord(int wordLength) {
 		return lineWidth + wordLength <= maxWidth;
 	}
@@ -35,48 +31,48 @@ public class Wrap {
 		return wordIterator < words.length;
 	}
 
-	private boolean isEnoghCharInLine() {
+	private boolean isEnoughCharInLine() {
 		return lineWidth >= minWidth;
-	}
-	
-	private void fillLine() {
-		while (isMoreWords() && isEnoughSpaceForWord(words[wordIterator].length())) {
-			newText += words[wordIterator] + " ";
-			lineWidth += words[wordIterator].length() + 1;
-			wordIterator++;
-		}
-	}
-
-	private void wrapSign(String word) {
-		newText += word.substring(0, maxWidth - lineWidth - 1) + "-\n";
-		if (isEnoughSpaceForWord(word.substring(maxWidth - lineWidth - 1).length())) {
-			newText += word.substring(maxWidth - lineWidth - 1) + " ";
-			lineWidth = word.substring(maxWidth - lineWidth - 1).length() + 1;
-			wordIterator++;
-		} else {
-			wrapSign(word.substring(maxWidth - lineWidth - 1));
-		}
 	}
 
 	private void newLine() {
-		newText += "\n";
+		newText.append("\n");
 		lineWidth = 0;
+	}
+
+	private void deleteLastSpaces() {
+		newText = new StringBuilder(newText.toString().replaceAll(" +$", ""));
+		newText = new StringBuilder(newText.toString().replaceAll(" +\n", "\n"));
+	}
+
+	private void addWord() {
+		String word = words[wordIterator];
+		newText.append(word + " ");
+		lineWidth += word.length() + 1;
+		wordIterator++;
+	}
+
+	private void wrapWord() {
+		String prefix = words[wordIterator].substring(0, maxWidth - lineWidth - 1);
+		String suffix = words[wordIterator].substring(maxWidth - lineWidth - 1);
+		newText.append(prefix + "-");
+		newLine();
+		words[wordIterator] = suffix;
 	}
 
 	public String wrapText(String rawText) {
 		initVariables(rawText);
-		do {
-			fillLine();
-			if (isMoreWords()) {
-				if (isEnoghCharInLine()) {
-					newLine();
-				} else {
-					wrapSign(words[wordIterator]);
-				}
+		while (isMoreWords()) {
+			if (isEnoughSpaceForWord(words[wordIterator].length())) {
+				addWord();
+			} else if (isEnoughCharInLine()) {
+				newLine();
+			} else {
+				wrapWord();
 			}
-			deleteLastSpaces();
-		} while (isMoreWords());
-		return newText;
+		}
+		deleteLastSpaces();
+		return newText.toString();
 	}
 
 }
